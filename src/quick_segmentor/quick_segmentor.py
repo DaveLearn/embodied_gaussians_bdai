d92bff00fc1996100190d714c173d2bbc54fcde7
David Pershouse <n11020211@qut.edu.au>
2025-07-07 10:32:54 +1000

    use new download location for segmenter

--- src/quick_segmentor/quick_segmentor.py
+++ src/quick_segmentor/quick_segmentor.py
@@ -1,13 +1,27 @@
 # Copyright (c) 2025 Boston Dynamics AI Institute LLC. All rights reserved.
 
+import os
 import numpy as np
 from isegm_gui import run_interactive_segmentor, load_model
+import urllib.request
 
 
 class QuickSegmentor:
     def __init__(self, device="cuda"):
         self.device = device
-        self.model = load_model("coco_lvis_h18_itermask.pth", device=self.device)
+        
+        # download the model if it doesn't exist
+        base_link = "https://github.com/hkchengrex/Cutie/releases/download/v1.0/"
+        checkpoint = "coco_lvis_h18_itermask.pth"
+        checkpoint_path = f"weights/{checkpoint}"
+        os.makedirs(os.path.dirname(checkpoint_path), exist_ok=True)
+        if not os.path.exists(checkpoint_path):
+            print(f"Downloading {checkpoint} to {checkpoint_path}...")
+            urllib.request.urlretrieve(f"{base_link}/{checkpoint}", checkpoint_path)
+        else:
+            print(f"Using existing {checkpoint} at {checkpoint_path}")
+
+        self.model = load_model(checkpoint, device=self.device)
 
     def segment_with_gui(self, image: np.ndarray) -> np.ndarray | None:
         """Blocks and returns the mask. Mask is None if no points are selected.
